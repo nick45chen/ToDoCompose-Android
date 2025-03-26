3e2e75ce69d3526719b5636fb392e9ccb63907ae
nickchen <nick45chen@gmail.com>
2025-03-26 14:46:48 +0800

    feat: add SharedViewModel and update ToDoRepository
    - Introduce `SharedViewModel` to manage and expose task data using `MutableStateFlow` and `StateFlow`.
    - Implement `getAllTask()` in `SharedViewModel` to collect and update all tasks from the repository.
    - Update `ToDoRepository` to be `ViewModelScoped`.

--- app/src/main/java/com/example/to_do_compose/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/example/to_do_compose/data/repository/ToDoRepository.kt
@@ -2,9 +2,11 @@ package com.example.to_do_compose.data.repository
 
 import com.example.to_do_compose.data.ToDoDao
 import com.example.to_do_compose.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
     val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
     val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
